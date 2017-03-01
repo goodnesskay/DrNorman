94ff4a93633c5e67a1ef448c2225d8f8009c7443
Phvash <davash001@gmail.com>
2017-03-01 02:54:58 +0100

    heroku setup

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_restful import Resource, Api, reqparse
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,6 +29,8 @@ api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
 	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 
 
 
