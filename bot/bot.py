7f57c8728803a393736bffad5f699b32d4eb4a2b
Phvash <davash001@gmail.com>
2017-03-01 03:08:05 +0100

    ip-port quick fix

--- bot/bot.py
+++ bot/bot.py
@@ -28,7 +28,6 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
-	app.run(debug=True)
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
 
